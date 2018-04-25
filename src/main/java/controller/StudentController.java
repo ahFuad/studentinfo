08e134694ed5d41a584b08b983e28188402214eb
FuadCharon <ahsanfuad@gmail.com>
2018-04-25 06:45:26 +0600

    initial commit

---
+++ src/main/java/controller/StudentController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import data.model.Student;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "studentController")
+@SessionScoped
+public class StudentController {
+    Student student1=new Student();
+    Student student2=new Student();
+
+    public StudentController() {
+    }
+
+    public Student getStudent1() {
+        return student1;
+    }
+
+    public void setStudent1(Student student1) {
+        this.student1 = student1;
+    }
+
+    public Student getStudent2() {
+        return student2;
+    }
+
+    public void setStudent2(Student student2) {
+        this.student2 = student2;
+    }
+    public String isNextPage(){
+        return "success";
+    }
+}
